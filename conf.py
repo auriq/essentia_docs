a98b4dd3f21c6cee8fef65418aec960925605422
ben waxer <bwaxer@auriq.com>
2015-09-24 14:54:35 -0700

    updating version number to 3.0.9

--- conf.py
+++ conf.py
@@ -52,7 +52,7 @@ copyright = u'2015, AuriQ Systems Inc.'
 # built documents.
 #
 # The short X.Y version.
-version = '3.0'
+version = '3.0.9'
 # The full version, including alpha/beta/rc tags.
 release = version
 
