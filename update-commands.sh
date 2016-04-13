d4a60dffada418aa8d53a6a9a414e351ecff6355
bwaxer <bwaxer@docdev.auriq.com>
2016-04-12 17:35:19 -0700

    text script keeping track of which commands to run to update docs

---
+++ update-commands.sh
@@ -0,0 +1,5 @@
+#git pull https://bwaxer:dragon911@github.com/auriq/essentia_docs.git beta-ess3.1.1
+#rm -R _build/html/*
+#make html
+#sudo cp -R _build/html/* /var/www/html/documentation/.
+#sudo cp source/screenshots/*.png /var/www/html/documentation/source/screenshots/
