e970ee89867a88487175bd878cb356872046c497
Ben Waxer <bwaxer@auriq.com>
2016-09-29 06:31:23 -0700

    ess man updates

---
+++ source/reference/manuals/ess.rst
@@ -0,0 +1,23 @@
+--------------------------------
+**ess select**
+--------------------------------
+
+:manpage:`ess-category(1)`
+:manpage:`ess-cat(1)`
+:manpage:`ess-cluster(1)`
+:manpage:`ess-create(1)`
+:manpage:`ess-drop(1)`
+:manpage:`ess-exec(1)`
+:manpage:`ess-file(1)`
+:manpage:`ess-lsa(1)`
+:manpage:`ess-ls(1)`
+:manpage:`ess-probe(1)`
+:manpage:`ess-purge(1)`
+:manpage:`ess-query(1)`
+:manpage:`ess-redshift(1)`
+:manpage:`ess-select(1)`
+:manpage:`ess-server(1)`
+:manpage:`ess-stream(1)`
+:manpage:`ess-summary(1)`
+:manpage:`ess-udbd(1)`
+:manpage:`ess-use(1)`
---
+++ source/reference/manuals/generate_essentia_man.sh
@@ -0,0 +1,33 @@
+essmain=`ess -h`
+
+confman="man_pages = [\n
+    ('ess', 'ess', u'',\n
+     [u'AuriQ Systems Inc.'], 1),\n"
+#echo -e $confman
+#'generate_essentia_ref'
+for file in `ls ess-*.rst | sed 's/.rst//g'`
+do
+ addtoess="\n:manpage:\`${file}(1)\`\n"
+ addtoconf="    ('$file', '$file', u'',\n
+     [u'AuriQ Systems Inc.'], 1),\n"
+ essmain="${essmain}${addtoess}"
+ confman="${confman}${addtoconf}"
+ # echo -e $file $confman
+done
+
+confman="$confman
+]\n
+\n
+# If true, show URL addresses after external links.\n
+#man_show_urls = False\n"
+echo -e $essmain > ess.rst
+echo -e $confman
+
+
+
+# remove old form of this from conf.py then run this script and copy the output to the end of conf.py
+# make man
+#ll /usr/share/man/man1/ | grep ess
+#sudo cp _build/man/ess* /usr/share/man/man1/.
+#man ess
+
